1ad952ab3ce1cb135db021240e86f7566a73634d
olanrewaju-07 <issabasheet@gmail.com>
2024-11-12 22:32:04 +0100

    RegisterPage

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 import SignupImage from "../assets/images/ak5.jpg"
 
 function Signin() {
-  return <div className="flex h-screen mobile:flex-col">
+  return <div className="flex h-screen tablet:flex-col">
     <div className="flex-1 items-center justify-center">
       <img src={SignupImage} alt="" className="w-full h-full object-cover" />
       </div>
       <div className="flex-1 flex items-center justify-center bg-white">
-        <form className="w-3/4 max-w-md space-y-4">
+        <div className="flex flex-col items-center gap-28 mobile:mt-5 mobile:gap-10 tablet:mt-5">
+          <h2 className="text-5xl text-semibold">BuyGun</h2>
+        <form className="w-4/4 max-w-md space-y-4 mobile:p-4">
           <h2 className="text-2xl font-semibold">Sign In</h2>
           <div className="flex flex-row items-center gap-2">
           <span>Don’t have an accout yet?</span>
@@ -27,6 +29,7 @@ function Signin() {
           </div>
           <Link to="/Landingpage" type="submit" className="flex items-center justify-center w-full p-2 bg-black text-white rounded">Sign in</Link>
         </form>
+        </div>
       </div>
 
   </div>;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 import SignupImage from "../assets/images/ak5.jpg"
 
 function Signup() {
-  return <div className="flex h-screen mobile:flex-col">
+  return <div className="flex h-screen tablet:flex-col">
     <div className="flex-1 items-center justify-center">
       <img src={SignupImage} alt="" className="w-full h-full object-cover" />
       </div>
       <div className="flex-1 flex items-center justify-center bg-white mobile:mt-5">
-        <form className="w-3/4 max-w-md space-y-4 mb-5">
+      <div className="flex flex-col items-center gap-20 mobile:gap-10 tablet:mt-5">
+      <h2 className="text-5xl text-semibold">BuyGun</h2>
+        <form className="w-4/4 max-w-md space-y-4 mb-5 mobile:p-4">
           <h2 className="text-2xl font-semibold">Sign Up</h2>
           <div className="flex flex-row items-center gap-2">
           <span>Already have an account?</span>
@@ -23,6 +25,7 @@ function Signup() {
           </div>
           <Link to="/Landingpage" type="submit" className=" flex justify-center items-center w-full p-2 bg-black text-white rounded">Sign up</Link>
         </form>
+      </div>
       </div>
 
   </div>;
