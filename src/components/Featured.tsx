19bb591d0accb7860580f6118466956e944766e7
olanrewaju-07 <issabasheet@gmail.com>
2024-12-07 09:40:32 +0100

    main

---
+++ src/components/Featured.tsx
@@ -0,0 +1,10 @@
+export default function Featured (){
+    return (
+        <div className="flex flex-col gap-4 items-center mt-5">
+            <div className="flex flex-row justify-between p-2 items-center">
+                <h2 className="font-semibold text-3xl">Featured</h2>
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
