699e4c787743f6912cc5a495f2670fe1867ba319
khaymanii <kennethakpo61@gmail.com>
2024-12-14 14:44:39 +0100

    finished product details page

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import Signin from "./pages/Signin";
 import LandingPage from "./pages/LandingPage";
 import Contactus from "./pages/Contactus";
 import Shop from "./pages/Shop";
+import ProductDetails from "./pages/ProductDetails";
 
 export default function App() {
   return (
     <div className="">
       <Routes>
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Signin" element={<Signin />} />
-        <Route path="/Contactus" element={<Contactus />} />
-        <Route path="/Shop" element={<Shop />} />
-
-        <Route path="/Landingpage" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/contact" element={<Contactus />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/landingpage" element={<LandingPage />} />
       </Routes>
     </div>
   );
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,5 +1,60 @@
+import Footer from "../components/Footer";
+
 function ProductDetails() {
-  return <div>ProductDetails</div>;
+  return (
+    <div>
+      {" "}
+      <div className="bg-black w-full h-64 flex items-center justify-center">
+        {" "}
+        <h1 className="font-semibold text-3xl text-center text-white">
+          Glock 19 Gen 5
+        </h1>
+      </div>
+      <div className="max-w-7xl mx-auto sm:flex lg:flex-row flex-col justify-between items-center">
+        <div className="sm:w-1/2 w-full">
+          <img
+            src="/assets/images/ak7bg.png"
+            alt="ak7bg"
+            className="w-[500px]]"
+          />
+        </div>
+        <div className="sm:w-1/2 lg:w-1/2 w-full">
+          <h1 className="text-4xl font-semibold lg:mb-4 mb-2 sm:mx-0 mx-2">
+            Glock 19 Gen 5
+          </h1>
+          <p className="font-semibold lg:mb-8 mb-2 text-2xl sm:mx-0 mx-2">
+            $499.9
+          </p>
+
+          <div className="flex flex-wrap items-center justify-between font-normal sm:text-xl lg:text-xl text-2xl lg:mb-8 mb-4 lg:flex-row sm:mx-0 mx-2">
+            <p>
+              Brand: <span className="font-semibold">Glock</span>
+            </p>
+            <p>
+              Category: <span className="font-semibold">Handgun</span>
+            </p>
+            <p>
+              Caliber: <span className="font-semibold">9mm</span>
+            </p>
+            <p>
+              Weight: <span className="font-semibold">670g</span>
+            </p>
+            <p>
+              Quantity in Stock: <span className="font-semibold">24</span>
+            </p>
+          </div>
+          <p className="sm:text-xl lg:text-xl text-2xl mb-8 sm:mx-0 mx-2">
+            The Glock 19 Gen 5 is a compact 9mm handgun perfect for personal
+            defense.
+          </p>
+          <button className="text-white bg-black px-4 py-3 w-[50%] sm:mx-0 mx-2 rounded-lg font-semibold">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default ProductDetails;
