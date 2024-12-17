d4c6731d9c8241fdcce73989cef17b3c3132b277
khaymanii <kennethakpo61@gmail.com>
2024-12-17 13:03:08 +0100

    added firebase logic

---
+++ src/firebaseConfig/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "buygun-ca397.firebaseapp.com",
+  projectId: "buygun-ca397",
+  storageBucket: "buygun-ca397.firebasestorage.app",
+  messagingSenderId: "823014984355",
+  appId: "1:823014984355:web:33d04a8cea61d3dd94cd16",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
