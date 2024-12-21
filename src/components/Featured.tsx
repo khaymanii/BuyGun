b4ac2f7b8db4b3d4b731a18ff6bf41995495584d
khaymanii <kennethakpo61@gmail.com>
2024-12-21 10:00:54 +0100

    added carts data to database

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,18 +1,50 @@
-import { featuredCard } from "../../Utils/Data";
+//import { featuredCard } from "../../Utils/Data";
 import { useCart } from "../context/CartContext";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig/firebase";
+import { useEffect, useState } from "react";
+
+type FeaturedCard = {
+  id: string;
+  image: string;
+  span: string;
+  button: string;
+  rate: string;
+  name: string;
+  description: string;
+  price: string;
+};
+
 export default function Featured() {
   const { addToCart } = useCart();
+  const [cards, setCards] = useState<FeaturedCard[]>([]);
 
+  useEffect(() => {
+    const fetchFeaturedCards = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "featureCard"));
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as FeaturedCard[];
+        setCards(data);
+      } catch (error) {
+        console.error("Error fetching featured cards:", error);
+      }
+    };
+
+    fetchFeaturedCards();
+  }, []);
   return (
     <div className="flex flex-col gap-4 mt-10 mb-10 max-w-7xl mx-auto w-full">
       <div className="flex flex-row text-center max-w-7xl mx-auto">
         <h2 className="font-semibold text-3xl">Featured</h2>
       </div>
       <div className="flex flex-wrap justify-between gap-5 items-center mobile:grid mobile:grid-cols-1 mobile:px-4 sm:grid-cols-2 md:gap-6 md:grid-cols-3">
-        {featuredCard.map((card, index) => (
+        {cards.map((card: FeaturedCard) => (
           <div
             className="flex flex-col bg-white rounded-lg shadow-md p-4 w-full sm:w-[300px] md:w-[250px] lg:w-[350px]"
-            key={index}
+            key={card.id}
           >
             <div className="relative flex flex-col items-center">
               <img
@@ -25,9 +57,16 @@ export default function Featured() {
               </span>
               <button
                 className="absolute bottom-4 left-4 bg-black text-white px-4 py-2 rounded-md opacity-90 mobile:px-3 mobile:py-1 text-xs"
-                onClick={addToCart}
+                onClick={() =>
+                  addToCart({
+                    id: card.id,
+                    name: card.name,
+                    price: card.price,
+                    image: card.image,
+                  })
+                }
               >
-                {card.button}
+                Add to Cart{" "}
               </button>
             </div>
             <div className="flex flex-col items-start mt-4">
